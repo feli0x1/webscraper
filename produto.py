f78ee18bd4ef1ca116a18cc8e058d8f6e324fa33
feli0x1 <sdiylaogzbhk@proton.me>
2022-12-28 22:19:53 -0300

    construtor redefinido, atributo 'tipo' removido da classe

--- produto.py
+++ produto.py
@@ -1,9 +1,8 @@
 class Produto:
-    def __init__(self, nome, preco, tipo, link):
-        self.nome = nome
-        self.preco = preco
-        self.tipo = tipo
-        self.link = link
+    def __init__(self):
+        self.nome = ''
+        self.preco = 0.0
+        self.link = ''
 
     def get_nome(self):
         return self.nome
@@ -16,15 +15,9 @@ class Produto:
 
     def set_preco(self, preco):
         self.preco = preco
-
-    def get_tipo(self):
-        return self.tipo
-
-    def set_tipo(self, tipo):
-        self.tipo = tipo
-
+    
     def get_link(self):
         return self.link
-        
+
     def set_link(self, link):
-        self.link = link
+        self.link = link        
